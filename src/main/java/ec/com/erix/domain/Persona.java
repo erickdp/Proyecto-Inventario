e78f76a089eff3cd2f5107356af774c1ce7faa93
Erick Díaz <erickdp@hotmail.com>
2021-05-13 12:47:47 -0500

    Clase Persona

--- src/main/java/ec/com/erix/domain/Persona.java
+++ src/main/java/ec/com/erix/domain/Persona.java
@@ -7,29 +7,41 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import lombok.*;
 
 /**
  *
  * @author Erick Diaz
  */
 @Entity
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "persona")
 public class Persona implements Serializable {
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_persona")
     private int idPersona;
+
+    @NotEmpty(message = "Cédula de identidad inválida")
+    private String ci;
+
+    @NotEmpty
+    private String nombre;
     
+    @NotEmpty
+    private String apellido;
     
+    @Email(message = "Debe ingresar un email valido")
+    private String email;
+    
+    private String telefono;
     
 }
