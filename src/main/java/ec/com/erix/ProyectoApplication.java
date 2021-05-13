c4fd5925b8788507a93fed75847f2b864ac6c2a5
Erick Díaz <erickdp@hotmail.com>
2021-05-13 12:08:13 -0500

    Initial commit

---
+++ src/main/java/ec/com/erix/ProyectoApplication.java
@@ -0,0 +1,13 @@
+package ec.com.erix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApplication.class, args);
+	}
+
+}
